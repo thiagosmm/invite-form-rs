98a79ad75aace5a4b5a17f55c731138ac5915703
thiagosmm <thiagosmmonteiro@gmail.com>
2025-01-22 16:18:41 -0300

    switch tipo e botao de aquivo

--- script.js
+++ script.js
@@ -10,3 +10,23 @@ function toggleStyle() {
     styleSpan.textContent = "Claro"
   }
 }
+
+function toggleType() {
+  const presencial = document.querySelector("#presencial");
+  const online = document.querySelector("#online");
+
+  const presencialImg = presencial.querySelector("img");
+  const onlineImg = online.querySelector("img");
+
+  if (presencial.classList.contains("selected")) {
+    presencial.classList.remove("selected");
+    presencialImg.src = "/assets/icons/building.png";
+    online.classList.add("selected");
+    onlineImg.src = "/assets/icons/video-selected.png";
+  } else {
+    presencial.classList.add("selected");
+    presencialImg.src = "/assets/icons/building-selected.png";
+    online.classList.remove("selected");
+    onlineImg.src = "/assets/icons/video.png"; 
+  }
+}
